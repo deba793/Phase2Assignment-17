66e55ca62722c03340bde97ee12d3ac01d0d142c
deba793 <debasisrout793@gmail.com>
2023-09-05 21:36:41 +0530

    Phase2Assignment-17

---
+++ Phase2Assignment-17/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Phase2Assignment_17.Models;
+
+namespace Phase2Assignment_17.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Phase2Assignment_17.Models.Movie> Movie { get; set; } = default!;
+    }
+}
